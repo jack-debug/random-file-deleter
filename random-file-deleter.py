e89ec8fa041354c55da13b7ee363fe146a729eec
jack-debug <55571956+jack-debug@users.noreply.github.com>
2019-10-29 15:25:25 +1100

    Update random-file-deleter.py

--- random-file-deleter.py
+++ random-file-deleter.py
@@ -7,3 +7,4 @@ while (count < 10):
 todelet = random.choice([x for x in os.listdir("C:\\") if os.path.isfile(os.path.join("C:\\", x))])
 count = count + 1
 os.remove(todelet)
+print(f"Removed {todelet}")
