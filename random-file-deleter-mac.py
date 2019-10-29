75da61d5efd0ded03d7c3bb46369995406bfdbe0
jack-debug <55571956+jack-debug@users.noreply.github.com>
2019-10-29 15:21:30 +1100

    Update random-file-deleter-mac.py

--- random-file-deleter-mac.py
+++ random-file-deleter-mac.py
@@ -2,8 +2,8 @@ import os
 import random
 
 _files = os.listdir('.')
-    number = random.randint(0, len(_files) - 1)
-     file_ = _files[number]
+number = random.randint(0, len(_files) - 1)
+file_ = _files[number]
 count = 0
 while (count < 1):
     count = count + 1
